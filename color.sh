94c634d56df6a6376de55c441f0ee894310c2ede
prippa <pavelrippa@gmail.com>
2018-03-15 19:28:48 +0200

    color sh change

--- color.sh
+++ color.sh
@@ -1,6 +1,10 @@
 gren=$(printf "[1;32mO\033[0m")
 yel=$(printf "[1;34mX\033[0m")
+cls=$(printf "\033[1J\033[;H")
+sle=$(sleep 1)
 
 sed "s|O|$gren|g" |
 sed "s|X|$yel|g" |
+sed "s|Plateau|$cls|g" |
+sed "g|Piece|$sle|" |
 sed "s|fin|player|g"
